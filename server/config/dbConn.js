13d26b20aec6aa3a90b76791bd79e4b80854c5a2
Djordje Cvetkovic <djordjeenglish@gmail.com>
2024-09-24 14:51:06 +0200

    deletes some unused code

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI, {
-      // useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DATABASE_URI);
   } catch (err) {
     console.error(err);
   }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 5500;
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
+const errorHandler = require("./middleware/errorHandler");
 
 connectDB();
 
@@ -13,9 +14,8 @@ app.use(express.json());
 // routes
 app.use("/register", require("./routes/register"));
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.use(errorHandler);
+
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
