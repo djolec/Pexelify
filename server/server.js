fb1055de4d8b449e140c41d280a1a4e55733e53b
Djordje Cvetkovic <djordjeenglish@gmail.com>
2024-09-24 15:27:56 +0200

    trying to resolve cors error, add cors middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const app = express();
 const PORT = process.env.PORT || 5500;
+const cors = require("cors");
+const credentials = require("./middleware/credentials");
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
 const errorHandler = require("./middleware/errorHandler");
 
 connectDB();
 
-const app = express();
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
 // built-in middleware for json
 app.use(express.json());
@@ -14,7 +22,7 @@ app.use(express.json());
 // routes
 app.use("/register", require("./routes/register"));
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
