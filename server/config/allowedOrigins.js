e7c064fdd55fce821ecdb81527356876bd563260
Djordje Cvetkovic <djordjeenglish@gmail.com>
2024-10-17 12:35:24 +0200

    removed all allowed origins except for the frontend domain

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,9 +1,3 @@
-const allowedOrigins = [
-  "https://pexelify.onrender.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3000",
-  "http://localhost:5173",
-  "http://localhost:5174",
-];
+const allowedOrigins = ["https://pexelify.onrender.com"];
 
 module.exports = allowedOrigins;
