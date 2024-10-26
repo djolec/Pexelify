fcce5ba8a9a95d571541fb8002f25a1a78ff5f20
Djordje Cvetkovic <djordjeenglish@gmail.com>
2024-10-26 02:47:49 +0200

    added optimistic updates to useSavePhoto

--- client/src/features/photos/useSavePhoto.js
+++ client/src/features/photos/useSavePhoto.js
@@ -4,7 +4,7 @@ import { useAuth } from "../../context/AuthContext";
 import toast from "react-hot-toast";
 
 const useSavePhoto = () => {
-  const { setAuth } = useAuth();
+  const { auth, setAuth } = useAuth();
   const serverAxios = useServerAxios();
 
   const savePhoto = (newPhoto) => {
@@ -13,18 +13,44 @@ const useSavePhoto = () => {
 
   const { mutate: addPhoto, isPending } = useMutation({
     mutationFn: (newPhoto) => savePhoto(newPhoto),
-    onSuccess: (data) => {
+    onMutate: (newPhoto) => {
+      const previousPhotos = auth.media.photos;
+
       setAuth((prev) => {
         return {
           ...prev,
           media: {
             ...prev.media,
-            photos: [...prev.media.photos, data.data.data],
+            photos: [...prev.media.photos, newPhoto],
+          },
+        };
+      });
+
+      return {
+        previousPhotos,
+      };
+    },
+    onSuccess: (data, newPhoto) => {
+      setAuth((prev) => {
+        return {
+          ...prev,
+          media: {
+            ...prev.media,
+            photos: prev.media.photos.map((photo) =>
+              photo.id === newPhoto.id ? data.data.data : photo
+            ),
           },
         };
       });
     },
-    onError: (err) => {
+
+    onError: (err, _newPhoto, context) => {
+      setAuth((prev) => {
+        return {
+          ...prev,
+          media: { ...prev.media, photos: context.previousPhotos },
+        };
+      });
       console.log("ERROR", err);
       if (err.status !== 403) return toast.error(err.response.data.error);
     },
