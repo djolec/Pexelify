5bbcbccc5d665e06325e5d00cdda12f8e17f1e7e
Djordje Cvetkovic <djordjeenglish@gmail.com>
2024-10-26 03:14:01 +0200

    added optimistic updates to useDeletePhoto

--- client/src/features/photos/useDeletePhoto.js
+++ client/src/features/photos/useDeletePhoto.js
@@ -4,7 +4,7 @@ import { useAuth } from "../../context/AuthContext";
 import toast from "react-hot-toast";
 
 const useDeletePhoto = () => {
-  const { setAuth } = useAuth();
+  const { auth, setAuth } = useAuth();
   const serverAxios = useServerAxios();
 
   const deletePhoto = (photoId) => {
@@ -13,19 +13,42 @@ const useDeletePhoto = () => {
 
   const { mutate: removePhoto, isPending } = useMutation({
     mutationFn: (photoId) => deletePhoto(photoId),
-    onSuccess: (data) => {
-      setAuth((prev) => {
-        const newPhotoArray = prev.media.photos.filter(
-          (photo) => photo._id !== data.data.data
-        );
+    onMutate: (photoId) => {
+      const previousPhotos = auth.media.photos;
 
+      setAuth((prev) => {
+        const newPhotoArray = auth.media.photos.filter(
+          (photo) => photo._id !== photoId
+        );
         return {
           ...prev,
           media: { ...prev.media, photos: newPhotoArray },
         };
       });
+
+      return {
+        previousPhotos,
+      };
     },
-    onError: (err) => {
+    // onSuccess: (data) => {
+    //   setAuth((prev) => {
+    //     const newPhotoArray = prev.media.photos.filter(
+    //       (photo) => photo._id !== data.data.data
+    //     );
+
+    //     return {
+    //       ...prev,
+    //       media: { ...prev.media, photos: newPhotoArray },
+    //     };
+    //   });
+    // },
+    onError: (err, _photoId, context) => {
+      setAuth((prev) => {
+        return {
+          ...prev,
+          media: { ...prev.media, photos: context.previousPhotos },
+        };
+      });
       console.log("ERROR", err);
       if (err.status !== 403) return toast.error(err.response.data.error);
     },
