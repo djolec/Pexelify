9a33359aa0ed2cae98db6ab58d0f6546dd35e40c
George <george86.dev@gmail.com>
2024-12-06 18:05:03 +0100

    Add loading page with logo while waiting to refresh the token

--- client/src/features/authentication/PersistLogin.jsx
+++ client/src/features/authentication/PersistLogin.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRefresh from "./useRefresh";
 import { useAuth } from "../../context/AuthContext";
+import { motion, AnimatePresence } from "framer-motion";
 import toast from "react-hot-toast";
 
 const PersistLogin = () => {
@@ -30,7 +31,28 @@ const PersistLogin = () => {
   return (
     <>
       {isLoading ? (
-        <div className="h-screen w-full bg-[var(--surface)]" />
+        <motion.div className="grid h-screen w-full place-content-center bg-[var(--surface)]">
+          <motion.img
+            initial={{ opacity: 1 }}
+            animate={{ scale: 1.4 }}
+            transition={{
+              repeatType: "reverse",
+              repeat: Infinity,
+              duration: 1,
+            }}
+            height="250"
+            width="250"
+            className="h-16 w-auto 2xl:h-[100px]"
+            src="/assets/logos/PexelifyLogo.webp"
+            alt="Pexelify logo"
+          />
+          <motion.p
+            initial={{ opacity: 1 }}
+            className="mt-2 text-2xl text-[var(--on-surface)]"
+          >
+            Loading...
+          </motion.p>
+        </motion.div>
       ) : (
         <Outlet />
       )}
